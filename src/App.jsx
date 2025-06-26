014470ca1e104d64c65d2363bbd04c99984228fe
Abhijeet Patade <pattadeabhijeet@gmail.com>
2025-06-26 12:49:28 +0530

    abhijeet second commit

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import CoursesPage from "./pages/CoursesPage";
 import ContactPage from "./pages/ContactPage";
 import './styles/Pages.css';
 import Admissions from "./pages/Admissions";
+import ChatbotComponent from "./components/Chatbot/ChatbotComponent";
 
 const App = ()=>{
   return(
@@ -18,6 +19,8 @@ const App = ()=>{
             <Route path="/contact" element={< ContactPage/>}/>
             <Route path="/admissions" element={<Admissions/>}/>
             </Routes>
+            
+            <ChatbotComponent/>
 
       </Router>
 
